2e2b15ec55db5ed460a50fef3c8fa87277d2e637
Ale <72107717+Ale-eth@users.noreply.github.com>
2022-05-05 19:21:55 -0300

    fin

--- Literals.java
+++ Literals.java
@@ -1,1 +1,35 @@
 
+// Tambien se conocen como Secuencias de escape o 'Escape sequences' en ingles
+
+public static void main(String[] args) {
+
+    System.out.println ("Hola Mundo. Estoy programando.");        // Original
+    // Hola Mundo. Estoy programando.
+  
+    System.out.println ("Hola Mundo. \nEstoy programando.");      // Salto de linea
+    // Hola Mundo.
+    // Estoy programando.
+  
+    System.out.println ("Hola Mundo. \n\tEstoy programando.");    // Un tabulado
+    // Hola Mundo.  Estoy programando.
+  
+    System.out.println ("Hola Mundo. \bEstoy programando.");      // Borra uno
+    // Hola Mundo.Estoy programando.
+  
+    System.out.println ("Hola Mundo. \rEstoy programando.");      // 'Carriage return' (Buscar ref en internet, no lo entendi 👀)
+    //  Estoy programando.
+  
+    System.out.println ("Hola Mundo. \'Estoy programando.");      // Inserta una comilla simple
+    //  Hola Mundo. 'Estoy programando.
+  
+    System.out.println ("Hola Mundo. \"Estoy programando.");      // Inserta una comilla doble
+    //  Hola Mundo. "Estoy programando.
+  
+    System.out.println ("Hola Mundo. \\Estoy programando.");      // Inserta una barra invertida
+    //  Hola Mundo. \Estoy programando.
+  
+    
+  
+    
+    
+}
